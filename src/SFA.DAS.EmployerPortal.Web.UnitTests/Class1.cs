677551d54eb51bbb213b367adb712f6a3b6de4ca
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2016-05-13 10:42:05 +0100

    Added in build script and also vstsconfig transofmration script. Updated the ports for the development and acceptance sites. Added in test class with one assertion.

---
+++ src/SFA.DAS.EmployerPortal.Web.UnitTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SFA.DAS.EmployerPortal.Web.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
